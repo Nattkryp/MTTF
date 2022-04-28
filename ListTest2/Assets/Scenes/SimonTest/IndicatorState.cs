537ae252930548f8eef0964d5b9c070751629ce4
Nattkryp <47904506+Nattkryp@users.noreply.github.com>
2022-04-28 23:31:38 +0200

    More test scene with lightning and new gfx assets

--- ListTest2/Assets/Scenes/SimonTest/IndicatorState.cs
+++ ListTest2/Assets/Scenes/SimonTest/IndicatorState.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class IndicatorState : MonoBehaviour
 {
     public GameObject machine;
     public SpriteRenderer body;
+
+    //Color indication
     public SpriteRenderer lightColor;
     public Sprite[] lights;
-    public AudioSource audio;
+    public Color[] lightColors;
+    public Light2D colorEmitted;        //works even if it doesn't seem to
+    public float lightIntensity = 1f;
+    public float blinkTimer;
+    float blinkIntervall = 1f;          //seconds
+    public bool doBlink;
+    public bool blinked;
+
     //Sprite indicatorBody;
     //Sprite greenLight;
     //Sprite redLight;
@@ -17,14 +27,68 @@ public class IndicatorState : MonoBehaviour
 
     private void Start()
     {
+        colorEmitted = GetComponent<Light2D>();
+        body.sprite = lights[0];
+
+        lightColors = new Color[5];
+        lightColors[0] = Color.cyan;        //invalid state
+        lightColors[1] = Color.green;        //running - green
+        lightColors[2] = Color.yellow;        //stopped - yellow
+        lightColors[3] = Color.blue;        //service - blue
+        lightColors[4] = Color.red;         //broken - red
+
         SetLight(2);
     }
 
+    private void Update()
+    {
+
+        FlashLight();
+    }
+
     public void SetLight(int lightState)
-        {
-        body.sprite = lights[0];
-        lightColor.sprite = lights[lightState];
-        audio.Play();
+        { if (doBlink != true) {
+            colorEmitted.intensity = 1f;
+            lightColor.sprite = lights[lightState];
+            colorEmitted.color = lightColors[lightState];
+
+            if (lightState == 4)
+            {
+                SetFlashing();
+            }
         }
+
+        }
+
+    public void SetFlashing() {
+        doBlink = true;
+        Debug.Log("SetFlashing called");
+    }
+
+    public void ReSetFlashing() {
+        doBlink = false;
+    }
+    public void FlashLight()
+    {
+        blinkTimer -= Time.deltaTime;
+
+
+        if (blinkTimer <= 0f && doBlink && !blinked)
+        {
+            blinkTimer = blinkIntervall;
+            //turn light on
+            colorEmitted.intensity = 5.0f;
+            Debug.Log("Blinklight on");
+            blinked = true;
+        }
+        else if (blinkTimer <= 0f && doBlink && blinked)
+        {
+            blinkTimer = blinkIntervall;
+            //turn light off
+            colorEmitted.intensity = 0.1f;
+            blinked = false;
+            Debug.Log("Blinklight off");
+        }
+    }
 }
 
--- ListTest2/Assets/Scenes/SimonTest/Machine.cs
+++ ListTest2/Assets/Scenes/SimonTest/Machine.cs
@@ -1,22 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering; //2019 VERSIONS
 
 public class Machine : MonoBehaviour
 {
 
-    [SerializeField] int _orderedState;
-    [SerializeField] int _laststate;
-    [SerializeField] int _state;    //1 = Started, 2 = Stopped, 3 = Service
+    //Statestuff
+    [SerializeField] int _orderedState; //used to try change the state
+    [SerializeField] int _laststate;    //used to evaluate vs _state to see if state changed
+    [SerializeField] int _state;        //1 = Started, 2 = Stopped, 3 = Service
+
+    //Machine Data
     [SerializeField] string _machineDescription;
 
+    //Functionality
     public IndicatorState indicatorLight;
 
+    public AudioSource soundClick;
+
+
+
+    private void Start()
+    {
+        if (GetComponent<AudioSource>() != null)
+        {
+            soundClick = GetComponent<AudioSource>();
+        }
+        else
+        {
+            Debug.LogWarning("No click sound component used");
+        }
+
+
+
+    }
 
     public void Update()
     {
         //Visuals
-        
+
         //Audio
 
         //Task stuff
@@ -27,49 +50,48 @@ public class Machine : MonoBehaviour
         OnStateChanged();
         _laststate = _state;
     }
-        void OnStateChanged() {
-            if (_laststate != _state)
+    void OnStateChanged()
+    {
+        if (_laststate != _state)
+        {
+            soundClick.Play();
+            if (_state == 1)          //started - green
             {
-                if (_state == 1)          //started - green
-                {
-                    indicatorLight.SetLight(1);
-                }
-                else if(_state == 2)          //stopped - yellow
-                {
-                    indicatorLight.SetLight(2);
-                }
-                else if (_state == 3)          //service blue
-                {
-                    indicatorLight.SetLight(3);
-                }
-                else if (_state == 4)          //stopped
-                {
-                    indicatorLight.SetLight(4);
-                }
-
-        }
-        }
-        
-
-         void SetState() {
-            //Set state if it haven't got a state
-            if (_state != _orderedState && _orderedState != 0)
-            {
-            _state = _orderedState;
-            _orderedState = 0;
-
-                Debug.Log("Machine state changed to" + _state.ToString());
+                indicatorLight.SetLight(1);
             }
-            else
+            else if (_state == 2)          //stopped - yellow
             {
+                indicatorLight.SetLight(2);
+            }
+            else if (_state == 3)          //service blue
+            {
+                indicatorLight.SetLight(3);
+            }
+            else if (_state == 4)          // broken - red
+            {
+                indicatorLight.SetLight(4);
+            }
+
+        }
+    }
+    void SetState()
+    {
+        //Set state if it haven't got a state
+        if (_state != _orderedState && _orderedState != 0)
+        {
+            SetOrderedState(_orderedState);
+            Debug.Log("Machine state changed to" + _state.ToString());
+        }
+        else
+        {
             return;
-            }
         }
-
-    public void SetOrderedState(int orderedState) {
+    }
+    public void SetOrderedState(int orderedState)
+    {
         _orderedState = 0;
-        _orderedState = orderedState;
+        _state = orderedState;
     }
 
-    
+
 }
