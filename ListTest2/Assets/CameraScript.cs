c256246d6c09007caf0af1123a2291835ac0ca76
Gogulus <62036185+Gogulus@users.noreply.github.com>
2022-05-01 15:08:18 +0200

    Added simple time management with + and - and * on Keypad
    + adds speed
    - subtract speed
    * restore normal speed

--- ListTest2/Assets/CameraScript.cs
+++ ListTest2/Assets/CameraScript.cs
@@ -13,7 +13,7 @@ public class CameraScript : MonoBehaviour
     }
     void Update()
     {
-
+        //Camera Movement
         if(Input.GetKey(KeyCode.A))
         {
             transform.position = new Vector3(transform.position.x + -baseScrollSpeed * Time.deltaTime, transform.position.y,transform.position.z);
@@ -31,6 +31,8 @@ public class CameraScript : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, transform.position.y + -baseScrollSpeed * Time.deltaTime, transform.position.z);
         }
+
+        //Camera Zoom
         if (Input.GetKey(KeyCode.Z))
         {
             cam.orthographicSize = cam.orthographicSize + 10 * baseScrollSpeed/0.67f * Time.deltaTime;
@@ -41,6 +43,22 @@ public class CameraScript : MonoBehaviour
             cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, 0.2f, 30f);
         }
 
+        //Manage time (Maybe not in this script in the future but would be good to have everything about player input in the same place)
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            Time.timeScale += 0.5f;
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            if(Time.timeScale > 0)
+            {
+                Time.timeScale -= 0.5f;
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.KeypadMultiply))
+        {
+            Time.timeScale = 1.0f;
+        }
 
     }
 }
